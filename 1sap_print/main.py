123055c0c8053ee54d24f050c547fa88707501aa
sakshi1901 <sakshi.patil4140@gmail.com>
2023-12-17 15:54:04 +0530

    print code updated main

--- 1sap_print/main.py
+++ 1sap_print/main.py
@@ -1,3 +1,11 @@
 n = 10
 
 print("Sakshi world" , n)
+print("Good Morning!!\nHave a nice day..")
+name = "Sakshi"
+age = "22"
+print(f"My name is {name}  I am {age} years old.")
+print("Hello" + " " + "sakshi")
+print(f"Hello, {name}!")
+print("This", "is", "on", "the", "first", "line", sep="-", end="\n")
+print("This", "is", "still", "on", "the", "first", "line.", sep="_")
