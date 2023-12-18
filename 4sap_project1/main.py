96391f72c923fb5ef499a432f5795715b0bb41e9
sakshi1901 <sakshi.patil4140@gmail.com>
2023-12-18 12:25:32 +0530

    project1 added main

---
+++ 4sap_project1/main.py
@@ -0,0 +1,7 @@
+print("Welcome to a band name generator!!!")
+
+country_name = input("In Which country did you grow up in?\n")
+
+pet_name = input("What is your pet's name?\n")
+
+print("Your band name can be " + country_name + " " + pet_name)
