e129c6377c3dae892061efd6b8563aef5e6c2a51
sakshi1901 <sakshi.patil4140@gmail.com>
2023-12-19 11:26:35 +0530

    updated file

--- 8sap_project2/main.py
+++ 8sap_project2/main.py
@@ -9,3 +9,5 @@ bill_per_person = total_bill / person
 final_amount = round(bill_per_person, 2)
 final_amount = "{:.2f}". format(bill_per_person)
 print(f"Each person should pay: ${final_amount}")
+
+
