73118a2fc4fa94fefd8a8f3840b8a893747f6913
sakshi1901 <sakshi.patil4140@gmail.com>
2023-12-20 08:24:03 +0530

    updated file

--- 16sap_banker_roulette/main.py
+++ 16sap_banker_roulette/main.py
@@ -1,9 +1,10 @@
 import random
 
 name = input("Enter everybody's name separated by comma: ")
-names_list=name.split(",")
+names_list = name.split(",")
 print(names_list)
 length = len(names_list)
 choice=random.randint(0, length-1)
 option=random.choice(names_list)
 print(f"{option} will pay the bill")
+
