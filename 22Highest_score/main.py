daaa1f01871df13bd770f69b149af31e006f14da
sakshi1901 <sakshi.patil4140@gmail.com>
2024-01-10 14:30:05 +0530

    file added

---
+++ 22Highest_score/main.py
@@ -0,0 +1,15 @@
+numbers = input("Enter list of numbers: ")
+
+numbers_list = numbers.split()
+count = 0
+for number in numbers_list:
+    count += 1
+    print(f"The length of the list is: {count}")
+
+for i in range(count):
+    numbers_list[i]=int(numbers_list[i])
+maximum_number = numbers_list[0]
+for number in numbers_list:
+    if number > maximum_number:
+        maximum_number=number
+print(f"The maximum number is: {maximum_number}")
